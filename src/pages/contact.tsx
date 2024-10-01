e08d880455b263c0b33b09197ce9a0558ecd7929
Asterki <asterki.dev@proton.me>
2024-09-30 21:56:00 -0600

    Feat: Contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,238 @@
+import * as React from 'react';
+import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faEnvelope,
+    faChevronCircleLeft,
+} from '@fortawesome/free-solid-svg-icons';
+import { faDiscord, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+import Particles from 'react-tsparticles';
+import type { Container, Engine } from 'tsparticles-engine';
+import { loadSlim } from 'tsparticles-slim';
+
+const ContactPage = () => {
+    const navigate = useNavigate();
+
+    const [isReady, setIsReady] = React.useState(false);
+    const [transitionTo, setTransitionTo] = React.useState('');
+
+    const particlesInit = React.useCallback(async (engine: Engine) => {
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = React.useCallback(
+        async (container: Container | undefined) => {
+            await console.log(container);
+        },
+        [],
+    );
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setIsReady(true);
+        }, 1000);
+    }, []);
+
+    React.useEffect(() => {
+        if (transitionTo !== '') {
+            setTimeout(() => {
+                if (transitionTo === 'home') {
+                    navigate('/');
+                } else {
+                    navigate(`/${transitionTo}`);
+                }
+            }, 1000);
+        }
+    }, [transitionTo]);
+
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center">
+            <div className="w-full top-0 left-0 z-50 absolute">
+                <motion.div
+                    className="bg-orange-500 min-h-screen w-full top-0 left-0 absolute shadow-md"
+                    variants={{
+                        initial: {
+                            minHeight: '100vh',
+                        },
+                        final: {
+                            minHeight: '11vh',
+                        },
+                    }}
+                    transition={{
+                        duration: 0.5,
+                        delay: 0.8,
+                        bounceDamping: 10,
+                        bounceStiffness: 100,
+                        ease: 'easeInOut',
+                    }}
+                    initial={isReady ? 'final' : 'initial'}
+                    animate={isReady ? 'final' : 'initial'}
+                ></motion.div>
+                <motion.div
+                    className="bg-cyan-500 min-h-screen w-full top-0 left-0 absolute shadow-md"
+                    variants={{
+                        initial: {
+                            minHeight: '100vh',
+                        },
+                        final: {
+                            minHeight: '10vh',
+                        },
+                    }}
+                    transition={{
+                        duration: 0.5,
+                        delay: 0.7,
+                        bounceDamping: 10,
+                        bounceStiffness: 100,
+                        ease: 'easeInOut',
+                    }}
+                    initial={isReady ? 'final' : 'initial'}
+                    animate={isReady ? 'final' : 'initial'}
+                ></motion.div>
+                <motion.div
+                    className="bg-green-500 min-h-screen w-full top-0 left-0 absolute shadow-md"
+                    variants={{
+                        initial: {
+                            minHeight: '100vh',
+                        },
+                        final: {
+                            minHeight: '10vh',
+                        },
+                    }}
+                    transition={{
+                        duration: 0.5,
+                        delay: 0.6,
+                        bounceDamping: 10,
+                        bounceStiffness: 100,
+                        ease: 'easeInOut',
+                    }}
+                    initial={isReady ? 'final' : 'initial'}
+                    animate={isReady ? 'final' : 'initial'}
+                ></motion.div>
+                <motion.div
+                    className="bg-rose-700 min-h-screen w-full top-0 left-0 absolute flex items-center justify-around p-4"
+                    variants={{
+                        initial: {
+                            minHeight: '100vh',
+                        },
+                        final: {
+                            minHeight: '10vh',
+                        },
+                        out: {
+                            minHeight: '100vh',
+                        },
+                    }}
+                    transition={{
+                        duration: 0.5,
+                        delay: isReady && transitionTo !== '' ? 0 : 0.5,
+                        bounceDamping: 10,
+                        bounceStiffness: 100,
+                        ease: 'easeInOut',
+                    }}
+                    initial={isReady ? 'final' : 'initial'}
+                    animate={
+                        isReady && transitionTo == '' ? 'final' : 'initial'
+                    }
+                >
+                    <motion.div className="flex-col flex items-center justify-center">
+                        <div className="bg-white md:w-2/12 w-11/12 h-2/12 rounded-full flex items-center justify-center">
+                            <img
+                                src="/assets/images/icon.png"
+                                alt="Icon"
+                                className="md:w-2/12 w-1/12 rounded-full"
+                            />
+                            <h1 className="text-center text-lg font-bold bg-clip-text text-transparent bg-gradient-to-tr from-rose-700 to-orange-500">
+                                Asterki Dev
+                            </h1>
+                        </div>
+
+                        <motion.p
+                            className="text-white text-center text-2xl mt-2 overflow-hidden"
+                            variants={{
+                                initial: {
+                                    opacity: 1,
+                                    height: '40px',
+                                },
+                                final: {
+                                    opacity: 0,
+                                    height: 0,
+                                },
+                            }}
+                            transition={{
+                                duration: 0.3,
+                                delay: 0.7,
+                                ease: 'easeInOut',
+                                onTransitionEnd: { display: 'none' },
+                            }}
+                            initial={isReady ? 'final' : 'initial'}
+                            animate={isReady ? 'final' : 'initial'}
+                        >
+                            Spark Curiosity
+                        </motion.p>
+                    </motion.div>
+                </motion.div>
+            </div>
+
+            <motion.div
+                className="flex items-center justify-center w-full flex-col opacity-0 z-10 mt-32"
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1, delay: 1.6 }}
+            >
+                <main className="flex md:flex-row flex-col gap-4 items-center justify-around md:mt-0 mt-24 mb-24 w-full">
+                    <section className="text-gray-700 flex items-center justify-center flex-col bg-white border-2 border-rose-500 p-6 rounded-md shadow-md w-11/12 md:w-9/12 text-center">
+                        <h1 className="text-center text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-tr from-rose-700 to-orange-500 w-full">
+                            Contact Me
+                        </h1>
+
+                        <ul className="my-4">
+                            <li>
+                                <FontAwesomeIcon icon={faEnvelope} /> My email
+                                is{' '}
+                                <a
+                                    href="mailto:asterki.dev@proton.me"
+                                    className="text-rose-500"
+                                >
+                                    asterki.dev@proton.me
+                                </a>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faInstagram} /> My Instagram
+                                is{' '}
+                                <a
+                                    href="https://instagram.com/nicht.fer"
+                                    className="text-rose-500"
+                                >
+                                    nicht.fer
+                                </a>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faDiscord} /> Talk to
+                                me via Discord: @asterki
+                            </li>
+                        </ul>
+
+                        <button
+                            className="bg-rose-700 text-white p-2 rounded-md hover:bg-white border-2 border-rose-700 hover:text-rose-700 transition-all flex items-center justify-center items-center gap-2"
+                            onClick={() => setTransitionTo('home')}
+                        >
+                            <FontAwesomeIcon icon={faChevronCircleLeft} />
+                            Return to the landing page
+                        </button>
+                    </section>
+                </main>
+            </motion.div>
+
+            <Particles
+                id="tsparticles"
+                className="absolute top-0 left-0 w-full h-full z-0"
+                url="/particleConfig.json"
+                init={particlesInit}
+                loaded={particlesLoaded}
+            />
+        </div>
+    );
+};
+
+export default ContactPage;
--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 // Pages
 import IndexPage from './pages/index';
 import AboutPage from './pages/about';
+import ContactPage from './pages/contact';
 
 const router = createBrowserRouter([
     {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
         path: '/about',
         element: <AboutPage />,
     },
+    {
+        path: "/contact",
+        element: <ContactPage />,
+    }
 ]);
 
 export default router;
