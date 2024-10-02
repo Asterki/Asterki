edb6e71dd8b62b6e16a862c879af00f9120bb705
Asterki <asterki.dev@proton.me>
2024-10-02 10:00:59 -0600

    Refactor: Extract Navbar component for reusability

---
+++ src/components/navbar.tsx
@@ -0,0 +1,139 @@
+import { motion } from 'framer-motion';
+
+interface NavbarProps {
+    isReady: boolean;
+    transitionTo: string;
+}
+
+const NavbarComponent: React.FC<NavbarProps> = (props: NavbarProps) => {
+    return (
+        <div className="w-full top-0 left-0 z-50 fixed">
+            <motion.div
+                className="bg-orange-500 min-h-screen w-full top-0 left-0 absolute shadow-md"
+                variants={{
+                    initial: {
+                        minHeight: '100vh',
+                    },
+                    final: {
+                        minHeight: '11vh',
+                    },
+                }}
+                transition={{
+                    duration: 0.5,
+                    delay: 0.8,
+                    bounceDamping: 10,
+                    bounceStiffness: 100,
+                    ease: 'easeInOut',
+                }}
+                initial={props.isReady ? 'final' : 'initial'}
+                animate={props.isReady ? 'final' : 'initial'}
+            ></motion.div>
+            <motion.div
+                className="bg-cyan-500 min-h-screen w-full top-0 left-0 absolute shadow-md"
+                variants={{
+                    initial: {
+                        minHeight: '100vh',
+                    },
+                    final: {
+                        minHeight: '10vh',
+                    },
+                }}
+                transition={{
+                    duration: 0.5,
+                    delay: 0.7,
+                    bounceDamping: 10,
+                    bounceStiffness: 100,
+                    ease: 'easeInOut',
+                }}
+                initial={props.isReady ? 'final' : 'initial'}
+                animate={props.isReady ? 'final' : 'initial'}
+            ></motion.div>
+            <motion.div
+                className="bg-green-500 min-h-screen w-full top-0 left-0 absolute shadow-md"
+                variants={{
+                    initial: {
+                        minHeight: '100vh',
+                    },
+                    final: {
+                        minHeight: '10vh',
+                    },
+                }}
+                transition={{
+                    duration: 0.5,
+                    delay: 0.6,
+                    bounceDamping: 10,
+                    bounceStiffness: 100,
+                    ease: 'easeInOut',
+                }}
+                initial={props.isReady ? 'final' : 'initial'}
+                animate={props.isReady ? 'final' : 'initial'}
+            ></motion.div>
+            <motion.div
+                className="bg-rose-700 min-h-screen w-full top-0 left-0 absolute flex items-center justify-around p-4"
+                variants={{
+                    initial: {
+                        minHeight: '100vh',
+                    },
+                    final: {
+                        minHeight: '10vh',
+                    },
+                    out: {
+                        minHeight: '100vh',
+                    },
+                }}
+                transition={{
+                    duration: 0.5,
+                    delay: props.isReady && props.transitionTo !== '' ? 0 : 0.5,
+                    bounceDamping: 10,
+                    bounceStiffness: 100,
+                    ease: 'easeInOut',
+                }}
+                initial={props.isReady ? 'final' : 'initial'}
+                animate={
+                    props.isReady && props.transitionTo == ''
+                        ? 'final'
+                        : 'initial'
+                }
+            >
+                <motion.div className="flex-col flex items-center justify-center">
+                    <div className="bg-white md:w-2/12 w-11/12 h-2/12 rounded-full flex items-center justify-center">
+                        <img
+                            src="/assets/images/icon.png"
+                            alt="Icon"
+                            className="md:w-2/12 w-1/12 rounded-full"
+                        />
+                        <h1 className="text-center text-lg font-bold bg-clip-text text-transparent bg-gradient-to-tr from-rose-700 to-orange-500">
+                            Asterki Dev
+                        </h1>
+                    </div>
+
+                    <motion.p
+                        className="text-white text-center text-2xl mt-2 overflow-hidden"
+                        variants={{
+                            initial: {
+                                opacity: 1,
+                                height: '40px',
+                            },
+                            final: {
+                                opacity: 0,
+                                height: 0,
+                            },
+                        }}
+                        transition={{
+                            duration: 0.3,
+                            delay: 0.7,
+                            ease: 'easeInOut',
+                            onTransitionEnd: { display: 'none' },
+                        }}
+                        initial={props.isReady ? 'final' : 'initial'}
+                        animate={props.isReady ? 'final' : 'initial'}
+                    >
+                        Spark Curiosity
+                    </motion.p>
+                </motion.div>
+            </motion.div>
+        </div>
+    );
+};
+
+export default NavbarComponent;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -18,6 +18,7 @@ import {
 import Particles from 'react-tsparticles';
 import type { Container, Engine } from 'tsparticles-engine';
 import { loadSlim } from 'tsparticles-slim';
+import NavbarComponent from '../components/navbar';
 
 const AboutPage = () => {
     const navigate = useNavigate();
@@ -52,135 +53,12 @@ const AboutPage = () => {
                 }
             }, 1000);
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [transitionTo]);
 
     return (
         <div className="min-h-screen flex flex-col items-center justify-center">
-            <div className="w-full top-0 left-0 z-50 sticky">
-                <motion.div
-                    className="bg-orange-500 min-h-screen w-full top-0 left-0 absolute shadow-md"
-                    variants={{
-                        initial: {
-                            minHeight: '100vh',
-                        },
-                        final: {
-                            minHeight: '11vh',
-                        },
-                    }}
-                    transition={{
-                        duration: 0.5,
-                        delay: 0.8,
-                        bounceDamping: 10,
-                        bounceStiffness: 100,
-                        ease: 'easeInOut',
-                    }}
-                    initial={isReady ? 'final' : 'initial'}
-                    animate={isReady ? 'final' : 'initial'}
-                ></motion.div>
-                <motion.div
-                    className="bg-cyan-500 min-h-screen w-full top-0 left-0 absolute shadow-md"
-                    variants={{
-                        initial: {
-                            minHeight: '100vh',
-                        },
-                        final: {
-                            minHeight: '10vh',
-                        },
-                    }}
-                    transition={{
-                        duration: 0.5,
-                        delay: 0.7,
-                        bounceDamping: 10,
-                        bounceStiffness: 100,
-                        ease: 'easeInOut',
-                    }}
-                    initial={isReady ? 'final' : 'initial'}
-                    animate={isReady ? 'final' : 'initial'}
-                ></motion.div>
-                <motion.div
-                    className="bg-green-500 min-h-screen w-full top-0 left-0 absolute shadow-md"
-                    variants={{
-                        initial: {
-                            minHeight: '100vh',
-                        },
-                        final: {
-                            minHeight: '10vh',
-                        },
-                    }}
-                    transition={{
-                        duration: 0.5,
-                        delay: 0.6,
-                        bounceDamping: 10,
-                        bounceStiffness: 100,
-                        ease: 'easeInOut',
-                    }}
-                    initial={isReady ? 'final' : 'initial'}
-                    animate={isReady ? 'final' : 'initial'}
-                ></motion.div>
-                <motion.div
-                    className="bg-rose-700 min-h-screen w-full top-0 left-0 absolute flex items-center justify-around p-4"
-                    variants={{
-                        initial: {
-                            minHeight: '100vh',
-                        },
-                        final: {
-                            minHeight: '10vh',
-                        },
-                        out: {
-                            minHeight: '100vh',
-                        },
-                    }}
-                    transition={{
-                        duration: 0.5,
-                        delay: isReady && transitionTo !== '' ? 0 : 0.5,
-                        bounceDamping: 10,
-                        bounceStiffness: 100,
-                        ease: 'easeInOut',
-                    }}
-                    initial={isReady ? 'final' : 'initial'}
-                    animate={
-                        isReady && transitionTo == '' ? 'final' : 'initial'
-                    }
-                >
-                    <motion.div className="flex-col flex items-center justify-center">
-                        <div className="bg-white md:w-2/12 w-11/12 h-2/12 rounded-full flex items-center justify-center">
-                            <img
-                                src="/assets/images/icon.png"
-                                alt="Icon"
-                                className="md:w-2/12 w-1/12 rounded-full"
-                            />
-                            <h1 className="text-center text-lg font-bold bg-clip-text text-transparent bg-gradient-to-tr from-rose-700 to-orange-500">
-                                Asterki Dev
-                            </h1>
-                        </div>
-
-                        <motion.p
-                            className="text-white text-center text-2xl mt-2 overflow-hidden"
-                            variants={{
-                                initial: {
-                                    opacity: 1,
-                                    height: '40px',
-                                },
-                                final: {
-                                    opacity: 0,
-                                    height: 0,
-                                },
-                            }}
-                            transition={{
-                                duration: 0.3,
-                                delay: 0.7,
-                                ease: 'easeInOut',
-                                onTransitionEnd: { display: 'none' },
-                            }}
-                            initial={isReady ? 'final' : 'initial'}
-                            animate={isReady ? 'final' : 'initial'}
-                        >
-                            Spark Curiosity
-                        </motion.p>
-                    </motion.div>
-                </motion.div>
-            </div>
+            <NavbarComponent isReady={isReady} transitionTo={transitionTo} />
 
             <motion.div
                 className="flex items-center justify-center w-full flex-col opacity-0 z-10 mt-32"
