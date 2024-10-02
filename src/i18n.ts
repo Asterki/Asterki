ed25b7b784504f8bae45e96a8c4a161eb5be7664
Asterki <asterki.dev@proton.me>
2024-10-02 10:54:19 -0600

    Feat: German translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -10,6 +10,7 @@ import enSkills from './locales/en/skills.json';
 import enContact from './locales/en/contact.json';
 import enProjects from './locales/en/projects.json';
 
+// Spanish language files
 import esNavbar from './locales/es/navbar.json';
 import esLanding from './locales/es/landing.json';
 import esAbout from './locales/es/about.json';
@@ -17,6 +18,15 @@ import esAboutAsterki from './locales/es/aboutasterki.json';
 import esSkills from './locales/es/skills.json';
 import esContact from './locales/es/contact.json';
 import esProjects from './locales/es/projects.json';
+
+// German 
+import deNavbar from './locales/de/navbar.json';
+import deLanding from './locales/de/landing.json';
+import deAbout from './locales/de/about.json';
+import deAboutAsterki from './locales/de/aboutasterki.json';
+import deSkills from './locales/de/skills.json';
+import deContact from './locales/de/contact.json';
+import deProjects from './locales/de/projects.json';
 
 const resources = {
     en: {
@@ -36,6 +46,15 @@ const resources = {
         skills: esSkills,
         contact: esContact,
         projects: esProjects,
+    },
+    de: {
+        navbar: deNavbar,
+        landing: deLanding,
+        about: deAbout,
+        aboutAsterki: deAboutAsterki,
+        skills: deSkills,
+        contact: deContact,
+        projects: deProjects,
     }
 };
 
