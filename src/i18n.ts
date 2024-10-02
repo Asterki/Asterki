0f71730774ef2db33fbfd14c71a12761e6e21ef6
Asterki <asterki.dev@proton.me>
2024-10-02 10:50:53 -0600

    Feat: Spanish translation

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-// Import the language files
+// Import the english language files
 import enNavbar from './locales/en/navbar.json';
 import enLanding from './locales/en/landing.json';
 import enAbout from './locales/en/about.json';
@@ -9,7 +9,14 @@ import enAboutAsterki from './locales/en/aboutasterki.json';
 import enSkills from './locales/en/skills.json';
 import enContact from './locales/en/contact.json';
 import enProjects from './locales/en/projects.json';
-// import fr from './locales/fr.json';
+
+import esNavbar from './locales/es/navbar.json';
+import esLanding from './locales/es/landing.json';
+import esAbout from './locales/es/about.json';
+import esAboutAsterki from './locales/es/aboutasterki.json';
+import esSkills from './locales/es/skills.json';
+import esContact from './locales/es/contact.json';
+import esProjects from './locales/es/projects.json';
 
 const resources = {
     en: {
@@ -21,6 +28,15 @@ const resources = {
         contact: enContact,
         projects: enProjects,
     },
+    es: {
+        navbar: esNavbar,
+        landing: esLanding,
+        about: esAbout,
+        aboutAsterki: esAboutAsterki,
+        skills: esSkills,
+        contact: esContact,
+        projects: esProjects,
+    }
 };
 
 i18n.use(initReactI18next) // passes i18n down to react-i18next
